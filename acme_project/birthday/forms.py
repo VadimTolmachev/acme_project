1d51047a0d15d7e01fafa372bc55c3ea0167e465
Толмачев Вадим <tolmachvv@yandex.ru>
2023-11-24 18:23:11 +0700

    edit forms and view

--- acme_project/birthday/forms.py
+++ acme_project/birthday/forms.py
@@ -2,6 +2,16 @@ from django import forms
 
 
 class BirthdayForm(forms.Form):
-    first_name = forms.CharField(max_length=20)
-    last_name = forms.CharField(required=False)
-    birthday = forms.DateField()
+    first_name = forms.CharField(
+        max_length=20,
+        label='Имя'
+    )
+    last_name = forms.CharField(
+        required=False,
+        label='Фамилия',
+        help_text='Необязательное поле'
+    )
+    birthday = forms.DateField(
+        label='Дата рождения',
+        widget=forms.DateInput(attrs={'type': 'date'})
+    )
--- acme_project/birthday/views.py
+++ acme_project/birthday/views.py
@@ -1,6 +1,29 @@
 from django.shortcuts import render
 
+from .forms import BirthdayForm
+
+
+def birthday1(request):
+    # Если есть параметры GET-запроса...
+    if request.GET:
+        # ...передаём параметры запроса в конструктор класса формы.
+        form = BirthdayForm(request.GET)
+        # Если данные валидны...
+        if form.is_valid():
+            # ...то считаем, сколько дней осталось до дня рождения.
+            pass
+    # Если нет параметров GET-запроса.
+    else:
+        # То просто создаём пустую форму.
+        form = BirthdayForm()
+    context = {'form': form}
+    return render(request, 'birthday/birthday.html', context)
+
 
 def birthday(request):
-    context = {}
-    return render(request, 'birthday/birthday.html', context=context)
+    # тоже самое что и birthday1 но короче.
+    form = BirthdayForm(request.GET or None)
+    if form.is_valid():
+        pass
+    context = {'form': form}
+    return render(request, 'birthday/birthday.html', context)
