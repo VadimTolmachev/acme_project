4cb3b2c9b3758c36c30c94ad421235db0bc20061
Вадим Толмачев <tolmachvv@ayndex.ru>
2023-11-25 13:48:57 +0700

    edit view

--- acme_project/birthday/views.py
+++ acme_project/birthday/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 
 from .forms import BirthdayForm
+from .utils import calculate_birthday_countdown
 
 
 def birthday1(request):
@@ -8,22 +9,33 @@ def birthday1(request):
     if request.GET:
         # ...передаём параметры запроса в конструктор класса формы.
         form = BirthdayForm(request.GET)
-        # Если данные валидны...
-        if form.is_valid():
-            # ...то считаем, сколько дней осталось до дня рождения.
-            pass
     # Если нет параметров GET-запроса.
     else:
         # То просто создаём пустую форму.
         form = BirthdayForm()
     context = {'form': form}
+    
+    # Если данные валидны...
+    if form.is_valid():
+        # ...то считаем, сколько дней осталось до дня рождения.
+        birthday_countdown = calculate_birthday_countdown(
+                form.cleaned_data['birthday']
+        )
+        context.update({'birthday_countdown': birthday_countdown})
     return render(request, 'birthday/birthday.html', context)
 
 
 def birthday(request):
     # тоже самое что и birthday1 но короче.
     form = BirthdayForm(request.GET or None)
-    if form.is_valid():
-        pass
     context = {'form': form}
+    # Если форма валидна...
+    if form.is_valid():
+        # ...вызовем функцию подсчёта дней:
+        birthday_countdown = calculate_birthday_countdown(
+            # ...и передаём в неё дату из словаря cleaned_data.
+            form.cleaned_data['birthday']
+        )
+        # Обновляем словарь контекста: добавляем в него новый элемент.
+        context.update({'birthday_countdown': birthday_countdown})
     return render(request, 'birthday/birthday.html', context)
